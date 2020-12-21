b4a9365058fda355bea2fa1b4a33ba3ed2949c32
Mecoly <Mecoly@github.com>
2020-12-21 11:12:18 +0800

    update Datatypes.c

--- C/source/DataTypes.c
+++ C/source/DataTypes.c
@@ -1,19 +1,16 @@
 #include<stdio.h>
 #include<limits.h>
+#include<float.h>
 int main(){
-  //printf("sizeof(byte):%d\n",sizeof(byte));
-  printf("sizeof(char):\t%d\n",sizeof(char));
-  printf("sizeof(short):\t%d\n",sizeof(short));
-  printf("sizeof(int):\t%d\n",sizeof(int));
-  printf("sizeof(long):\t%d\n",sizeof(long));
-  //printf("sizeof(long int):\t%d\n",sizeof(long int));
-  //printf("sizeof(long long):\t%d\n",sizeof(long long));
-  //printf("sizeof(long long int):%d\n",sizeof(long long int));
-  printf("LONG_MAX:\t%d\n",LONG_MAX);
-  printf("LLONG_MAX:\t%d\n",LLONG_MAX);
-  printf("int\n");
-  printf("sizeof(int):%d\n",sizeof(int));
-  printf("INT_MIN:%d\n",INT_MIN);
-  printf("INT_MAX:%d\n",INT_MAX);
+  enum season {spring = 1, summer,autumn,winter};
+  printf("sizeof(char):\t\t%d\trange:%d - %d\n",sizeof(char),CHAR_MIN,CHAR_MAX);
+  printf("sizeof(short):\t\t%d\trange:%d - %d\n",sizeof(short),SHRT_MIN,SHRT_MAX);
+  printf("sizeof(int):\t\t%d\trange:%d - %d\n",sizeof(int),INT_MIN,INT_MAX);
+  printf("sizeof(long):\t\t%d\trange:%ld - %ld\n",sizeof(long),LONG_MIN,LONG_MAX);
+  printf("sizeof(long long):\t%d\trange:%ld - %ld\n",sizeof(long long),LLONG_MIN,LLONG_MAX);
+  printf("sizeof(float):\t\t%d\trange:%e - %e\n",sizeof(float),FLT_MIN,FLT_MAX);
+  printf("sizeof(double):\t\t%d\trange:%e - %e\n",sizeof(double),DBL_MIN,DBL_MAX);
+  printf("sizeof(long double):\t%d\trange:%e - %e\n",sizeof(long double),LDBL_MIN,LDBL_MAX);
+  printf("spring:\t%d\tsummer:\t%d\tautumn:\t%d\twinter:\t%d\n",spring,summer,autumn,winter);
   return 0;
 }
