0a913fd97ee51e9828f89f4f64c7ca6c78ef1b2e
changjianchen <changjian.chen@testandard.cn>
2020-12-22 17:07:26 +0800

    Update More.c

--- C/source/More.c
+++ C/source/More.c
@@ -1,97 +1,97 @@
 #include<stdio.h>
 #include<stdlib.h>
-void change_value(int a,int b);
-void change_pvalue(int* a,int* b);
-int main(){
+void change_value(int a, int b);
+void change_pvalue(int* a, int* b);
+int main() {
   int a = 4;
   int b = 5;
-  int* p =NULL;
+  int* p = NULL;
   int* ptemp = NULL;
   printf("========================================\n");
-  printf("p=%p\n",p);
+  printf("p=%p\n", p);
   //printf("*p=%d\n",*p);  //运行时：段错误
-  p=&a;
+  p = &a;
   printf("p=&a;\n");
-  printf("*p=%d\n",*p);
-  printf("p=%p\n",p);
-  printf("sizeof(p)=%d\n",sizeof(p));
-  printf("sizeof(*p)=%d\n",sizeof(*p));
+  printf("*p=%d\n", *p);
+  printf("p=%p\n", p);
+  printf("sizeof(p)=%d\n", sizeof(p));
+  printf("sizeof(*p)=%d\n", sizeof(*p));
   printf("========================================\n");
-  printf("main:\ta=%d\tb=%d\n",a,b);
-  change_value(a,b);
-  printf("main:\ta=%d\tb=%d\n",a,b);
-  change_pvalue(&a,&b);
-  printf("main:\ta=%d\tb=%d\n",a,b);
+  printf("main:\ta=%d\tb=%d\n", a, b);
+  change_value(a, b);
+  printf("main:\ta=%d\tb=%d\n", a, b);
+  change_pvalue(&a, &b);
+  printf("main:\ta=%d\tb=%d\n", a, b);
   printf("========================================\n");
-  p=(int*)calloc(10,sizeof(int));
-  if(p){
+  p = (int*)calloc(10, sizeof(int));
+  if (p) {
     printf("p=(int*)calloc(10,sizeof(int));\n");
-    printf("p=%p\n",p);
-    printf("*p=%d\n",*p);
-    printf("p+1=%p\n",p+1);
-    printf("*(p+1)=%d\n",*(p+1));
-    printf("&p[1]=%p\n",&p[1]);
-    printf("p[1]=%d\n",p[1]);
+    printf("p=%p\n", p);
+    printf("*p=%d\n", *p);
+    printf("p+1=%p\n", p + 1);
+    printf("*(p+1)=%d\n", *(p + 1));
+    printf("&p[1]=%p\n", &p[1]);
+    printf("p[1]=%d\n", p[1]);
     free(p);
   }
-  else{
+  else {
     printf("Error:calloc false\n");
-    printf("p=%p\n",p);
+    printf("p=%p\n", p);
   }
   printf("========================================\n");
-  p=(int*)malloc(10*sizeof(int));
-  if(p){
+  p = (int*)malloc(10 * sizeof(int));
+  if (p) {
     printf("p=(int*)malloc(10*sizeof(int));\n");
-    printf("p=%p\n",p);
-    printf("*p=%d\n",*p);
-    printf("p+1=%p\n",p+1);
-    printf("*(p+1)=%d\n",*(p+1));
-    printf("&p[1]=%p\n",&p[1]);
-    printf("p[1]=%d\n",p[1]);
+    printf("p=%p\n", p);
+    printf("*p=%d\n", *p);
+    printf("p+1=%p\n", p + 1);
+    printf("*(p+1)=%d\n", *(p + 1));
+    printf("&p[1]=%p\n", &p[1]);
+    printf("p[1]=%d\n", p[1]);
     free(p);
   }
-  else{
+  else {
     printf("Error:malloc false\n");
-    printf("p=%p\n",p);
+    printf("p=%p\n", p);
   }
   printf("========================================\n");
-  p=(int*)malloc(10*sizeof(int));
-  if(p){
+  p = (int*)malloc(10 * sizeof(int));
+  if (p) {
     printf("p=(int*)malloc(10*sizeof(int));\n");
-    printf("p=%p\n",p);
-    printf("*p=%d\n",*p);
-    ptemp=(int*)realloc(p,10*sizeof(int));
-    if(ptemp){
+    printf("p=%p\n", p);
+    printf("*p=%d\n", *p);
+    ptemp = (int*)realloc(p, 10 * sizeof(int));
+    if (ptemp) {
       printf("ptemp=(int*)realloc(p,10*sizeof(int));\n");
-      printf("p=%p\n",p);
-      printf("ptemp=%p\n",ptemp);
-      printf("*ptemp=%d\n",*ptemp);
+      printf("p=%p\n", p);
+      printf("ptemp=%p\n", ptemp);
+      printf("*ptemp=%d\n", *ptemp);
       free(ptemp);
     }
-    else{
+    else {
       printf("Error:realloc false\n");
-      printf("p=%p\n",p);
-      printf("*p=%d\n",*p);
-      printf("ptemp=%p\n",ptemp);
+      printf("p=%p\n", p);
+      printf("*p=%d\n", *p);
+      printf("ptemp=%p\n", ptemp);
       free(p);
     }
   }
-  else{
+  else {
     printf("Error:malloc false\n");
-    printf("p=%p\n",p);
+    printf("p=%p\n", p);
   }
   printf("========================================\n");
   return 0;
 }
-void change_value(int a,int b){
+void change_value(int a, int b) {
   int temp = a;
   a = b;
   b = temp;
-  printf("change_value:\ta=%d\tb=%d\n",a,b);
+  printf("change_value:\ta=%d\tb=%d\n", a, b);
 }
-void change_pvalue(int* a,int* b){
+void change_pvalue(int* a, int* b) {
   int temp = *a;
   *a = *b;
   *b = temp;
-  printf("change_pvalue:\ta=%d\tb=%d\n",*a,*b);
+  printf("change_pvalue:\ta=%d\tb=%d\n", *a, *b);
 }
